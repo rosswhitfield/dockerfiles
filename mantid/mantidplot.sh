21392fb010011dea8d387cf357aa9711c1230fa5
Ross Whitfield <whitfieldre@ornl.gov>
2015-02-24 09:18:43 -0500

    Keep same hostname.

--- mantid/mantidplot.sh
+++ mantid/mantidplot.sh
@@ -1,4 +1,5 @@
 docker run -it \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -e DISPLAY=unix$DISPLAY \
-    mantid
+       -v /tmp/.X11-unix:/tmp/.X11-unix \
+       --hostname=$HOSTNAME \
+       -e DISPLAY=unix$DISPLAY \
+       mantid
